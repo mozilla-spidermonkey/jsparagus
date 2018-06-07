8e6f143ed81090e48a10906541df18954de51b5d
Jason Orendorff <jason.orendorff@gmail.com>
2018-06-07 09:19:04 -0500

    Change lexer to prefer longer matches over shorter ones.

--- lexer.py
+++ lexer.py
@@ -3,35 +3,51 @@
 import re
 
 class LexicalGrammar:
-    def __init__(self, tokens, **regexps):
+    def __init__(self, tokens, ignore=r'[ \t]*', **regexps):
         token_list = sorted(tokens.split(), key=len, reverse=True)
+        self.ignore_re = re.compile(ignore)
         self.token_re = re.compile("|".join(re.escape(token) for token in token_list))
         self.parser_pairs = [(k, re.compile(v)) for k, v in regexps.items()]
 
     def __call__(self, source):
-        return Tokenizer(self.token_re, self.parser_pairs, source)
+        return Tokenizer(self.ignore_re, self.token_re, self.parser_pairs, source)
 
 
 class Tokenizer:
-    def __init__(self, token_re, parser_pairs, source):
+    def __init__(self, ignore_re, token_re, parser_pairs, source):
+        self.ignore_re = ignore_re
         self.token_re = token_re
         self.parser_pairs = parser_pairs
         self.src = source
         self.point = 0
 
     def _match(self):
-        while self.point < len(self.src) and self.src[self.point] in " \t":
-            self.point += 1
+        ignore_match = self.ignore_re.match(self.src, self.point)
+        if ignore_match is not None:
+            self.point = ignore_match.end()
+
         if self.point == len(self.src):
             return None
-        match = self.token_re.match(self.src, self.point)
-        if match is not None:
-            return match.group(0), match
+
+        # Try the token_re.
+        token_match = self.token_re.match(self.src, self.point)
+
+        # Try all the parser_pairs.
         for name, pattern in self.parser_pairs:
             match = pattern.match(self.src, self.point)
             if match is not None:
-                return name, match
-        raise ValueError("unexpected characters {!r}".format(self.src[self.point:self.point+4]))
+                break
+        else:
+            name = match = None
+
+        if match is not None and token_match is not None and match.end() > token_match.end():
+            return name, match
+        elif token_match is not None:
+            return token_match.group(0), token_match
+        elif match is not None:
+            return name, match
+        else:
+            raise ValueError("unexpected characters {!r}".format(self.src[self.point:self.point+4]))
 
     def peek(self):
         hit = self._match()
