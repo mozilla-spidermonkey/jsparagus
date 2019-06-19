61065adbdc21c57b608bf2bc4eefc71b606fadd3
Jason Orendorff <jason.orendorff@gmail.com>
2019-06-19 08:48:03 -0700

    Get pgen.py working again.

--- pgen.py
+++ pgen.py
@@ -10,15 +10,18 @@ import parse_pgen
 import gen
 import sys
 
+
 def main():
     parser = argparse.ArgumentParser(description="Generate a parser.")
+    parser.add_argument('--target', choices=['python', 'rust'], default='rust',
+                        help="target language to use when printing the parser tables")
     parser.add_argument('grammar', metavar='FILE', nargs=1,
                         help=".pgen file containing the grammar")
-    args = parser.parse_args()
+    options = parser.parse_args()
 
-    [pgen_filename] = args.grammar
-    grammar = parse_pgen.load_grammar(pgen_filename)
-    gen.generate_parser(sys.stdout, grammar, next(iter(grammar)))
+    [pgen_filename] = options.grammar
+    grammar, goal_nts = parse_pgen.load_grammar(pgen_filename)
+    gen.generate_parser(sys.stdout, grammar, goal_nts, target=options.target)
 
 
 if __name__ == '__main__':
