be64b884c57b11505e6a2629935a7d32760bc023
Jason Orendorff <jason.orendorff@gmail.com>
2019-06-28 16:56:47 -0500

    Run tests at the end of update.sh.

--- update.sh
+++ update.sh
@@ -8,3 +8,4 @@ mv parse_pgen_generated_NEW.py parse_pgen_generated.py
 python3 parse_pgen.py  # run tests
 python3 pgen.py --target=rust pgen.pgen > client/src/parser_generated.rs
 (cd client && cargo build)
+python3 test.py
