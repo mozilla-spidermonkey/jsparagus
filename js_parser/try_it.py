b93457afb8ad110f073d65c37a33096c0d287dc8
Jason Orendorff <jason.orendorff@gmail.com>
2019-07-16 13:14:35 -0500

    Support passing a filename to js_parser.try_it on the command line.

--- js_parser/try_it.py
+++ js_parser/try_it.py
@@ -5,6 +5,7 @@
 See README.md for instructions.
 """
 
+import argparse
 from .parser import JSParser
 from jsparagus.lexer import SyntaxError
 
@@ -17,8 +18,8 @@ def interactive_input(parser, prompt="js> "):
             return parser.close()
         prompt = "..> "
 
-
-def main():
+def rpl():
+    """Read-print loop."""
     while True:
         parser = JSParser()
         try:
@@ -30,6 +31,24 @@ def main():
             print(exc.__class__.__name__ + ": " + str(exc))
             continue
         print(result)
+
+def main():
+    parser = argparse.ArgumentParser(description="Try out the JS parser.")
+    parser.add_argument('input_file', metavar='FILE', nargs='?',
+                        help=".js file to parse")
+    options = parser.parse_args()
+
+    if options.input_file is not None:
+        with open(options.input_file) as f:
+            source = f.readlines()
+        parser = JSParser()
+        for line in source:
+            print(line.rstrip())
+            parser.write(line)
+        ast = parser.close()
+        print(ast)
+    else:
+        rpl()
 
 
 if __name__ == '__main__':
