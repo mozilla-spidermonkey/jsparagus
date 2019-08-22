a6223991fb3142725ad57b10b14c3385bddadf71
Tom Schuster <evilpies@gmail.com>
2019-08-23 01:34:46 +0200

    improve dis by skipping operands

--- rust/emitter/src/dis.rs
+++ rust/emitter/src/dis.rs
@@ -1,19 +1,32 @@
-use crate::opcode::Opcode;
+use crate::opcode::{Opcode, LENGTH};
 use std::convert::TryFrom;
 use std::fmt::Write;
 
 /// Return a string form of the given bytecode.
 pub fn dis(bc: &[u8]) -> String {
     let mut result = String::new();
-    for &byte in bc {
-        match Opcode::try_from(byte) {
-            Ok(op) => {
-                writeln!(&mut result, "{:?}", op).unwrap();
-            }
-            Err(()) => {
-                writeln!(&mut result, "{}", byte).unwrap();
-            }
+    let mut iter = bc.iter();
+    loop {
+        let len = match iter.next() {
+            Some(byte) => match Opcode::try_from(*byte) {
+                Ok(op) => {
+                    write!(&mut result, "{:?}", op).unwrap();
+                    LENGTH[op as usize] - 1
+                }
+                Err(()) => {
+                    write!(&mut result, "{}", byte).unwrap();
+                    0
+                }
+            },
+            None => break,
+        };
+
+        for _ in 0..len {
+            write!(&mut result, " {}", iter.next().unwrap()).unwrap();
         }
+
+        writeln!(&mut result).unwrap();
     }
+
     result
 }
