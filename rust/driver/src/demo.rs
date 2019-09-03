d4bea4c241f78f3efa67f82cf7b87a482b02491f
Jason Orendorff <jason.orendorff@gmail.com>
2019-09-03 10:28:23 -0500

    driver: Print some stats at the end of a run.

--- rust/driver/src/demo.rs
+++ rust/driver/src/demo.rs
@@ -11,40 +11,95 @@ use ast;
 use emitter::emit;
 use parser::parse_script;
 
-fn parse_file(path: &Path) -> io::Result<()> {
+#[derive(Clone, Debug, Default)]
+pub struct DemoStats {
+    files_attempted: usize,
+    files_parsed: usize,
+
+    /// Total size of all the files attempted, in bytes.
+    total_bytes: u64,
+}
+
+impl DemoStats {
+    pub fn new() -> DemoStats {
+        DemoStats::default()
+    }
+
+    pub fn new_single(size_bytes: u64, success: bool) -> DemoStats {
+        DemoStats {
+            files_attempted: 1,
+            files_parsed: if success { 1 } else { 0 },
+            total_bytes: size_bytes,
+        }
+    }
+
+    pub fn add(&mut self, other: &DemoStats) {
+        self.files_attempted += other.files_attempted;
+        self.files_parsed += other.files_parsed;
+        self.total_bytes += other.total_bytes;
+    }
+}
+
+/// Try parsing a file.
+///
+/// Returns an Err only if opening or reading the file fails;
+/// parse errors are simply printed to stdout.
+fn parse_file(path: &Path, size_bytes: u64) -> io::Result<DemoStats> {
     print!("{}:", path.display());
     io::stdout().flush()?;
     let contents = match fs::read_to_string(path) {
         Err(err) => {
             println!(" error reading file: {}", err);
-            return Ok(());
+            return Ok(DemoStats::new_single(size_bytes, false));
         }
         Ok(s) => s,
     };
     let result = parse_script(&contents);
+    let stats = DemoStats::new_single(size_bytes, result.is_ok());
     match result {
         Ok(_ast) => println!(" ok"),
         Err(err) => println!(" error: {}", err.message()),
     }
-    Ok(())
+    Ok(stats)
 }
 
-pub fn parse_file_or_dir(filename: &impl AsRef<OsStr>) -> io::Result<()> {
-    let path = Path::new(filename);
-    if path.is_dir() {
-        for entry in fs::read_dir(&path)? {
-            let file = entry?.path();
-            if file.is_file() && file.extension() == Some(OsStr::new("js")) {
-                parse_file(&file)?;
-            }
-            if file.is_dir() {
-                parse_file_or_dir(&file)?;
-            }
-        }
-    } else {
-        parse_file(Path::new(filename))?;
+/// Try parsing all the files in a directory, recursively.
+///
+/// Returns an Err only if reading a file or directory fails;
+/// parse errors are simply printed to stdout.
+fn parse_dir(path: &Path) -> io::Result<DemoStats> {
+    let mut summary = DemoStats::new();
+    for entry_result in fs::read_dir(&path)? {
+        let entry = entry_result?;
+        let file = entry.path();
+        let metadata = entry.metadata()?;
+        let stats = if metadata.is_file() {
+            parse_file(&file, metadata.len())?
+        } else if metadata.is_dir() {
+            parse_dir(&file)?
+        } else {
+            DemoStats::new()
+        };
+        summary.add(&stats);
     }
-    Ok(())
+    Ok(summary)
+}
+
+/// Try parsing a file, or all the files in a directory recursively.
+///
+/// Returns an Err only if reading a file or directory fails;
+/// parse errors are simply printed to stdout.
+pub fn parse_file_or_dir(filename: &impl AsRef<OsStr>) -> io::Result<DemoStats> {
+    let path = Path::new(filename);
+    let metadata = path.metadata()?;
+    if metadata.is_dir() {
+        parse_dir(path)
+    } else {
+        // No `if metadata.is_file()` here, we instead try opening it and let
+        // that fail if this is some exotic filesystem thingy. That way the
+        // user gets an error message.
+        parse_file(Path::new(filename), metadata.len())
+    }
 }
 
 fn run(buffer: &str) {
--- rust/driver/src/main.rs
+++ rust/driver/src/main.rs
@@ -6,11 +6,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     match args.len() {
         1 => demo::read_print_loop(),
-        2 => {
-            if let Err(err) = demo::parse_file_or_dir(&args[1]) {
+        2 => match demo::parse_file_or_dir(&args[1]) {
+            Ok(stats) => {
+                println!("{:#?}", stats);
+            }
+            Err(err) => {
                 eprintln!("{}", err);
             }
-        }
+        },
         _ => eprintln!("usage: parser [FILE/DIR]"),
     }
 }
