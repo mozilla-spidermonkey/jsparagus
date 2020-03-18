865d300b6038652b489bb868f12035ab35ec9fe7
Tom Schuster <evilpies@gmail.com>
2020-03-18 21:35:29 +0100

    Make interpreter crate compile again

--- crates/interpreter/src/evaluate.rs
+++ crates/interpreter/src/evaluate.rs
@@ -54,7 +54,7 @@ impl Helpers for EmitResult {
 
     fn read_atom(&self, offset: usize) -> String {
         let index = self.atoms[self.read_i32(offset) as usize];
-        self.all_atoms[index.into()].clone()
+        self.all_atoms[usize::from(index)].clone()
     }
 }
 
