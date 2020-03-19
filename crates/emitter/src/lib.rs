144fed0d8349e83ac35d03d39de702fb40b48ded
Tooru Fujisawa <arai_a@mac.com>
2020-03-19 11:02:57 +0900

    Fix emitter crate test

--- crates/emitter/src/lib.rs
+++ crates/emitter/src/lib.rs
@@ -40,19 +40,25 @@ mod tests {
     use super::{emit, EmitOptions};
     use crate::dis::*;
     use crate::opcode::*;
+    use ast::source_atom_set::SourceAtomSet;
     use bumpalo::Bump;
     use parser::{parse_script, ParseOptions};
+    use std::cell::RefCell;
+    use std::rc::Rc;
 
     fn bytecode(source: &str) -> Vec<u8> {
         let alloc = &Bump::new();
         let parse_options = ParseOptions::new();
-        let parse_result = parse_script(alloc, source, &parse_options).expect("Failed to parse");
+        let atoms = Rc::new(RefCell::new(SourceAtomSet::new()));
+        let parse_result =
+            parse_script(alloc, source, &parse_options, atoms.clone()).expect("Failed to parse");
         // println!("{:?}", parse_result);
 
         let emit_options = EmitOptions::new();
         let bc = emit(
             &mut ast::types::Program::Script(parse_result.unbox()),
             &emit_options,
+            atoms.replace(SourceAtomSet::new_uninitialized()),
         )
         .expect("Should work!")
         .bytecode;
