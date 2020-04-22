2fd1a2a561395a325c7f0f5caca331b5fe93ea56
Zibi Braniecki <zibi@braniecki.net>
2020-04-22 11:35:05 -0700

    Add jquery 3.5.0 slim to parser benches

--- crates/parser/benches/parser.rs
+++ crates/parser/benches/parser.rs
@@ -7,17 +7,21 @@ use std::rc::Rc;
 
 use bumpalo::Bump;
 use jsparagus_ast::source_atom_set::SourceAtomSet;
-use jsparagus_parser::{parse_script, ParseOptions};
 use jsparagus_ast::source_slice_list::SourceSliceList;
+use jsparagus_parser::{parse_script, ParseOptions};
 
 fn parser_bench(c: &mut Criterion) {
-    let tests = &["simple", "__finStreamer-proto"];
+    let tests = &["simple", "__finStreamer-proto", "jquery-slim"];
     let mut programs = HashMap::new();
 
     programs.insert("simple", include_str!("./simple.js"));
     programs.insert(
         "__finStreamer-proto",
         include_str!("./__finStreamer-proto.js"),
+    );
+    programs.insert(
+        "jquery-slim",
+        include_str!("./jquery-3.5.0.slim.min.js"),
     );
 
     c.bench_function_over_inputs(
