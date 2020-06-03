8597e9ff5673a8009caf66a852adcbd1c766f4da
Tooru Fujisawa <arai_a@mac.com>
2020-06-03 14:28:23 +0900

    Collect information about Annex B.3.3
    In scope pass, collect functions that can be Annex B, in each scope,
    and at `var`-level scope, check if each function is Annex B or not,
    and mark Annex B functions.

--- crates/stencil/src/function.rs
+++ crates/stencil/src/function.rs
@@ -320,6 +320,10 @@ impl FunctionStencil {
         self.name = Some(name);
     }
 
+    pub fn name<'a>(&'a self) -> &'a Option<SourceAtomSetIndex> {
+        &self.name
+    }
+
     pub fn set_has_rest(&mut self) {
         self.lazy_script_mut().set_has_rest();
     }
@@ -378,6 +382,10 @@ impl FunctionStencilList {
         let index = self.items.len();
         self.items.push(fun_data);
         FunctionStencilIndex::new(index)
+    }
+
+    pub fn get<'a>(&'a self, index: FunctionStencilIndex) -> &'a FunctionStencil {
+        &self.items[usize::from(index)]
     }
 
     pub fn get_mut<'a>(&'a mut self, index: FunctionStencilIndex) -> &'a mut FunctionStencil {
