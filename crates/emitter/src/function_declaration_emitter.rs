28bdfb3846a224002f6ba8fdcb5c2826ac8ea09e
Tooru Fujisawa <arai_a@mac.com>
2020-06-03 17:13:25 +0900

    Support Anned B.3.3 and lexical function (fixes #537, fixes #538)

--- crates/emitter/src/function_declaration_emitter.rs
+++ crates/emitter/src/function_declaration_emitter.rs
@@ -1,4 +1,7 @@
 use crate::ast_emitter::AstEmitter;
+use crate::emitter::EmitError;
+use crate::reference_op_emitter::{AssignmentEmitter, DeclarationEmitter, NameReferenceEmitter};
+use ast::source_atom_set::SourceAtomSetIndex;
 use stencil::function::FunctionStencilIndex;
 use stencil::gcthings::GCThingIndex;
 
@@ -12,13 +15,53 @@ impl LazyFunctionEmitter {
     }
 }
 
-pub struct FunctionDeclarationEmitter {
+pub struct TopLevelFunctionDeclarationEmitter {
     pub fun: GCThingIndex,
 }
 
-impl FunctionDeclarationEmitter {
+impl TopLevelFunctionDeclarationEmitter {
     pub fn emit(self, emitter: &mut AstEmitter) {
         emitter.emit.lambda(self.fun);
         emitter.emit.def_fun();
+    }
+}
+
+pub struct LexicalFunctionDeclarationEmitter {
+    pub name: SourceAtomSetIndex,
+    pub fun: GCThingIndex,
+}
+
+impl LexicalFunctionDeclarationEmitter {
+    pub fn emit(self, emitter: &mut AstEmitter) -> Result<(), EmitError> {
+        DeclarationEmitter {
+            lhs: |emitter| {
+                Ok(NameReferenceEmitter { name: self.name }.emit_for_declaration(emitter))
+            },
+            rhs: |emitter| {
+                emitter.emit.lambda(self.fun);
+                Ok(())
+            },
+        }
+        .emit(emitter)
+    }
+}
+
+pub struct AnnexBFunctionDeclarationEmitter {
+    pub name: SourceAtomSetIndex,
+    pub fun: GCThingIndex,
+}
+
+impl AnnexBFunctionDeclarationEmitter {
+    pub fn emit(self, emitter: &mut AstEmitter) -> Result<(), EmitError> {
+        AssignmentEmitter {
+            lhs: |emitter| {
+                Ok(NameReferenceEmitter { name: self.name }.emit_for_assignment(emitter))
+            },
+            rhs: |emitter| {
+                emitter.emit.lambda(self.fun);
+                Ok(())
+            },
+        }
+        .emit(emitter)
     }
 }
